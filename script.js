6d0f100f15a890d524d1511530560a213411c65c
Durgesh Kumar Yadav <57389386+DGSH9@users.noreply.github.com>
2022-01-18 23:08:03 +0530

    todo-with-js-completed

---
+++ script.js
@@ -0,0 +1,9 @@
+const addTodoButton = document.getElementById("button1");
+const inputField = document.getElementById("input1");
+const todoContainer = document.getElementById("todo-container");
+
+addTodoButton.addEventListener("click", function () {
+    var p = document.createElement('p');
+    p.innerText = inputField.value;
+    todoContainer.appendChild(p);
+})
